348b46835cf55868355c440aa942d2fe8ddf1877
Katka Marcincakova <katulik137@gmail.com>
2023-11-16 21:26:51 +0100

    color text set up

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+            xxs: '340px',
+            xs: '410px',
+            sm: '480px',
+            s: '590px',
+            md: '720px',
+            m: '850px',
+            lg: '976px',
+            l: '1200px',
+            xl: '1440px'
+        },
+        extend: {
+            colors: {
+                primary: 'hsl(238, 100%, 15%)',
+				secondary: 'hsl(0, 0%, 36%)',
+				tertiary: 'hsl(236, 6%, 46%)',
+				contrast: 'hsl(226, 56%, 40%)',
+				bg: 'hsl(0, 0%, 100%)',
+            }
+        },
 	},
 	plugins: [],
 }
