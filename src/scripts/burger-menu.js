f618aee8fe6847982301079af441ae430ec75104
Katka Marcincakova <katulik137@gmail.com>
2023-11-16 22:22:25 +0100

    navbar done

---
+++ src/scripts/burger-menu.js
@@ -0,0 +1,25 @@
+﻿const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function toggleMobileMenu() {
+    btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+
+    var navbarHeight = document.getElementById('navbar').offsetHeight;
+    var x = 1 * navbarHeight;
+    document.getElementById('menu').style.top = x + 'px';
+}
+
+btn.addEventListener('click', () => {
+    toggleMobileMenu();
+})
+
+function scrollToSection(section) {
+    $('html').animate({
+        scrollTop: $(section).offset().top - 1.9 * ($("#navbar").height())
+    }, 100);
+
+    if (!$('#menu').hasClass('hidden')) {
+        toggleMobileMenu();
+    }
+};
